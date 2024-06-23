f7c455dd4473332c931702164d341034b3d09cb0
PimKalasinmongkol <144045585+PimKalasinmongkol@users.noreply.github.com>
2024-06-23 16:41:28 +0700

    Add files via upload

---
+++ ESP32CAM_Car1/ESP32CAM_Car1.ino
@@ -0,0 +1,324 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT
+//#define CAMERA_MODEL_M5STACK_PSRAM
+#define CAMERA_MODEL_AI_THINKER
+
+const char* ssid = "Pim";   //Enter SSID WIFI Name
+const char* password = "12345678";   //Enter WIFI Password
+String serverName = "192.168.74.170";
+String serverPath = "/ESP32CAM/upload_img.php";
+const int serverPort = 80;
+
+unsigned long previousMillis = 0; 
+const int Interval = 5000;
+
+WiFiClient client;
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    21
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      19
+#define Y4_GPIO_NUM      18
+#define Y3_GPIO_NUM       5
+#define Y2_GPIO_NUM       4
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
+
+
+#elif defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#else
+#error "Camera model not selected"
+#endif
+
+// GPIO Setting
+extern int gpLb =  2; // Left 1
+extern int gpLf = 14; // Left 2
+extern int gpRb = 15; // Right 1
+extern int gpRf = 13; // Right 2
+extern int motion_s =  4; // MotionSensor
+extern int ir_s =  12; // InfraredSersor
+extern String WiFiAddr ="";
+hw_timer_t *My_timer = NULL;
+
+void startCameraServer();
+
+
+void sendPhotoToServer() {
+  String AllData;
+  String DataBody;
+
+  Serial.println();
+  Serial.println("-----------");
+  Serial.println("Taking a photo...");
+
+  /*if (LED_Flash_ON == true) {
+    digitalWrite(FLASH_LED_PIN, HIGH);
+    delay(1000);
+  }*/
+  
+  for (int i = 0; i <= 3; i++) {
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();
+     if(!fb) {
+        Serial.println("Camera capture failed");
+        Serial.println("Restarting the ESP32 CAM.");
+        delay(1000);
+        ESP.restart();
+        return;
+      } 
+    esp_camera_fb_return(fb);
+    delay(200);
+  }
+  
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    Serial.println("Restarting the ESP32 CAM.");
+    delay(1000);
+    ESP.restart();
+    return;
+  } 
+
+  //if (LED_Flash_ON == true) digitalWrite(FLASH_LED_PIN, LOW);
+  
+  Serial.println("Taking a photo was successful.");
+
+
+  Serial.println("Connecting to server: " + serverName);
+
+  if (client.connect(serverName.c_str(), serverPort)) {
+    Serial.println("Connection successful!");   
+     
+    String post_data = "--dataMarker\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"ESP32CAMCap.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String head =  post_data;
+    String boundary = "\r\n--dataMarker--\r\n";
+    
+    uint32_t imageLen = fb->len;
+    uint32_t dataLen = head.length() + boundary.length();
+    uint32_t totalLen = imageLen + dataLen;
+    
+    client.println("POST " + serverPath + " HTTP/1.1");
+    client.println("Host: " + serverName);
+    client.println("Content-Length: " + String(totalLen));
+    client.println("Content-Type: multipart/form-data; boundary=dataMarker");
+    client.println();
+    client.print(head);
+  
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n=0; n<fbLen; n=n+1024) {
+      if (n+1024 < fbLen) {
+        client.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen%1024>0) {
+        size_t remainder = fbLen%1024;
+        client.write(fbBuf, remainder);
+      }
+    }   
+    client.print(boundary);
+    
+    esp_camera_fb_return(fb);
+   
+    int timoutTimer = 10000;
+    long startTimer = millis();
+    boolean state = false;
+    Serial.println("Response : ");
+    while ((startTimer + timoutTimer) > millis()) {
+      Serial.print(".");
+      delay(200);
+         
+      // Skip HTTP headers   
+      while (client.available()) {
+        char c = client.read();
+        if (c == '\n') {
+          if (AllData.length()==0) { state=true; }
+          AllData = "";
+        }
+        else if (c != '\r') { AllData += String(c); }
+        if (state==true) { DataBody += String(c); }
+        startTimer = millis();
+      }
+      if (DataBody.length()>0) { break; }
+    }
+    client.stop();
+    Serial.println(DataBody);
+    Serial.println("-----------");
+    Serial.println();
+    
+  }
+  else {
+    client.stop();
+    DataBody = "Connection to " + serverName +  " failed.";
+    Serial.println(DataBody);
+    Serial.println("-----------");
+  }
+}
+
+/*void IRAM_ATTR onTimer() {
+ digitalWrite(2, !digitalRead(2));
+}*/
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  
+  
+
+  pinMode(gpLb, OUTPUT); //Left Backward
+  pinMode(gpLf, OUTPUT); //Left Forward
+  pinMode(gpRb, OUTPUT); //Right Forward
+  pinMode(gpRf, OUTPUT); //Right Backward
+  pinMode(motion_s, INPUT); // MotionSensor
+  pinMode(ir_s, INPUT); // InfraredSersor
+
+  //initialize
+  digitalWrite(gpLb, LOW);
+  digitalWrite(gpLf, LOW);
+  digitalWrite(gpRb, LOW);
+  digitalWrite(gpRf, LOW);
+  digitalWrite(motion_s, LOW);
+  digitalWrite(ir_s, HIGH);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  //drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_CIF);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("Connecting to : ");
+  Serial.println(ssid);
+
+  int connecting_process_timed_out = 20; //--> 20 = 20 seconds.
+  connecting_process_timed_out = connecting_process_timed_out * 2;
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+    if(connecting_process_timed_out > 0) connecting_process_timed_out--;
+    if(connecting_process_timed_out == 0) {
+      Serial.println();
+      Serial.print("Failed to connect to ");
+      Serial.println(ssid);
+      Serial.println("Restarting the ESP32 CAM.");
+      delay(1000);
+      ESP.restart();
+    }
+  }
+  
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  WiFiAddr = WiFi.localIP().toString();
+  Serial.println("' to connect");
+  
+  /*My_timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(My_timer, &sendPhotoToServer, true);
+  timerAlarmWrite(My_timer, 200000, true);
+  timerAlarmEnable(My_timer);*/
+}
+
+void loop() {
+  /*int DAC_Motion = digitalRead(motion_s);
+  int DAC_IR = digitalRead(ir_s);
+
+  if (motion_s == HIGH) {//ตรวจพบ
+    Serial.println(DAC_Motion);
+  } else {
+    Serial.println(DAC_Motion);
+  }
+  if (ir_s == LOW) {//ตรวจพบ
+    Serial.println(DAC_IR);
+  } else {
+    Serial.println(DAC_IR);
+  }*/
+  /*unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= Interval) {
+    previousMillis = currentMillis;
+    
+    sendPhotoToServer();
+  }*/
+   
+  
+}
